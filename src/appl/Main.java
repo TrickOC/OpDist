ea988894a480507bf9a2d6b703fdc372b9aec08b
Patrick Oliveira do Carmo <patrick.oliveira.carmo@gmail.com>
2022-08-08 09:40:49 -0300

    Add reduce method

--- src/appl/Main.java
+++ src/appl/Main.java
@@ -1,34 +1,52 @@
 package appl;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> input = new LinkedList<>();
-        Random r = new Random();
-        long tempo_single, tempo_multi, soma;
+        Random r = new Random(4228);
+        long initial, soma;
+        long[] tempos = new long[4];
 
-        for (int i = 0; i < 10000000; ++i)
-            input.add(r.nextInt(1000));
+        for (int i = 0; i < 1000000; ++i)
+            input.add(r.nextInt(100));
 
         System.out.println("Soma single thread com map e sum:");
-        tempo_single = System.nanoTime();
+        initial = System.nanoTime();
         soma = input.stream().mapToLong(Integer::longValue).sum();
-        tempo_single = System.nanoTime() - tempo_single;
+        tempos[0] = System.nanoTime() - initial;
         System.out.println("Total da soma: " + soma);
-        System.out.println("Tempo da soma: " + tempo_single + "ns");
+        System.out.println("Tempo da soma: " + tempos[0] + "ns");
         System.out.println();
 
         System.out.println("Soma multi threads com map e sum:");
-        tempo_multi = System.nanoTime();
+        initial = System.nanoTime();
         soma = input.parallelStream().mapToLong(Integer::longValue).sum();
-        tempo_multi = System.nanoTime() - tempo_multi;
+        tempos[1] = System.nanoTime() - initial;
         System.out.println("Total da soma: " + soma);
-        System.out.println("Tempo da soma: " + tempo_multi + "ns");
-
+        System.out.println("Tempo da soma: " + tempos[1] + "ns");
         System.out.println();
-        System.out.println("Diferenca de tempo(Multi - Single): " + (tempo_multi - tempo_single) + "ns");
+
+        System.out.println("Soma single thread com reduce e sum:");
+        initial = System.nanoTime();
+        soma = input.stream().reduce(0, Integer::sum);
+        tempos[2] = System.nanoTime() - initial;
+        System.out.println("Total da soma: " + soma);
+        System.out.println("Tempo da soma: " + tempos[2] + "ns");
+        System.out.println();
+
+        System.out.println("Soma multi threads com reduce e sum:");
+        initial = System.nanoTime();
+        soma = input.parallelStream().reduce(0, Integer::sum);
+        tempos[3] = System.nanoTime() - initial;
+        System.out.println("Total da soma: " + soma);
+        System.out.println("Tempo da soma: " + tempos[3] + "ns");
+        System.out.println();
+
+        int idx_menor = (int) LongStream.range(0, tempos.length).reduce(0,Math::min);
+        System.out.println("Menor tempo: " + tempos[idx_menor] + "ns");
+
     }
 }
